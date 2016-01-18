edd51e9f79e9150fcd7310eda5561913ef83efee
Tremas <AWS2-04@HPi5-04.infodom.web>
2016-01-18 19:10:11 +0100

    medioacabado
    medioacabado

--- index.php
+++ index.php
@@ -14,37 +14,72 @@
     <h1>Agenda de Eventos</h1>
     
   </div>
-  <div class="row">
-     <div class="col-sm-3">
-      <h3>Lunes </h3>
-      <p>evento 1</p>
-     
-    </div>
-    <div class="col-sm-3">
-      <h3>Martes </h3>
-      <p>evento 1</p>
-    </div>
-    <div class="col-sm-3">
-      <h3>Miercoles</h3>        
-      <p>evento 1</p>
-    </div>
-	 <div class="col-sm-3">
-      <h3>Jueves</h3>
-     <p>evento 1</p>
-    </div>
-    <div class="col-sm-3">
-      <h3>Viernes</h3>
-      
-    </div>
-    <div class="col-sm-3">
-      <h3>Sabado</h3>        
-      
-    </div>
-	  <div class="col-sm-3">
-      <h3>Domingo</h3>        
-      
-    </div>
-  </div>
+  <div class="panel panel-primary">
+	<div class="panel-heading">INICIO</div>
+	<div class="panel-body">
+	  <div class="row">
+		<div class="col-sm-3">
+			<div class="panel panel-warning">
+				<div class="panel-heading"><h3>Lunes <h3 id=dialunes>1</h3></h3></div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="panel panel-warning">
+				<div class="panel-heading"> <h3>Martes <h3 id=diamartes>2</h3> </h3></div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div>   
+		</div>
+		<div class="col-sm-3">
+			<div class="panel panel-warning">
+				<div class="panel-heading">  <h3>Miercoles <h3 id=diamiercoles>3</h3></h3>     </div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div> 
+		</div>
+		<div class="col-sm-3">
+			<div class="panel panel-warning">
+				<div class="panel-heading">  <h3>Jueves<h3 id=diajueves>4</h3></h3>     </div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div> 
+		</div>
+		<div class="col-sm-3">
+			<div class="panel panel-warning">
+				<div class="panel-heading"> <h3>Viernes<h3 id=diaviernes>5</h3></h3></div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div> 
+		</div>
+		<div class="col-sm-3">
+		<div class="panel panel-warning">
+				<div class="panel-heading">  <h3>Sabado<h3 id=diasabado>6</h3></h3>     </div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div>       
+		</div>
+		<div class="col-sm-3">
+		<div class="panel panel-warning">
+				<div class="panel-heading">  <h3>Domingo<h3 id=diadomingo>7</h3></h3>     </div>
+				<div class="panel-body">
+					<p>evento 1</p>
+				</div>
+			</div> 
+		  
+		  
+		</div>
+	  </div>
+	</div>
+	<button type="button" class="btn btn-success center-block">Semana Anterior</button><button type="button" class="btn btn-danger center-block">Semana Siguiente</button>
+	</div>
 </div>
 
 </body>
